c0a4e4ae565313c2574d4f761c98a6d59ea31795
Beka-Gogberashvili <bekagogberashvili@redberry.ge>
2023-02-03 18:42:02 +0400

    landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="min-h-[100vh] bg-shutterstock">
+    <nav class="mx-[70px] pt-[25px]">
+      <img src="@/assets/img/redberry-logo.png" alt="logo" class="" />
+      <div class="h-px bg-[#191919] w-full mt-6 mb-10" />
+    </nav>
+
+    <div class="flex justify-center mt-[424px]">
+      <RouterLink to="/personal">
+        <button
+          class="bg-[#191919] px-[126px] py-[18px] text-white rounded-lg font-helvetica text-xl hover:bg-[#2c2c2c] active:bg-[#3d3d3d]"
+        >
+          რეზიუმეს დამატება
+        </button>
+        <img
+          src="@/assets/img/dta.png"
+          alt="dta"
+          class="top-[473px] right-[565px] z-[-1] absolute"
+        />
+      </RouterLink>
+    </div>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        shutterstock: "url('/src/assets/img/shutterstock.png')",
+      },
+      fontFamily: {
+        helvetica: ["Helvetica Neue Bold Caps", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
